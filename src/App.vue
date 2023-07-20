b31ceeb993557c1a70ad8c0c52900eb0711718c8
simadivdar <simadivdar@gmail.com>
2023-07-20 08:23:21 +0330

    start&installation

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header class="bg-green-500">
+    <h1 class="text-3xl font-bold underline bg-green-500">
+      Hello ToDo!
+    </h1>
+    <button class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">TailwindTest</button>
+    <div>
+      <HelloWorld msg="You did it!" />
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+<style scoped>
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default{
+    content:["./src/**/*.html", "./src/**/*.vue",],
+     darkMode: false, // or 'media' or 'class'
+     theme: {
+       extend: {},
+     },
+     plugins: [],
+   }
