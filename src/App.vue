de69aeec67c986813f3b4ab2f90b02413a1ce652
simadivdar <simadivdar@gmail.com>
2023-07-20 14:22:22 +0330

    addReadingTodo

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import ReadTodoList from "./components/ReadTitle.vue";
+export default {
+  name: "App",
+  components: {
+    ReadTodoList
+  }
+};
 </script>
-
 <template>
-  <header class="bg-green-500">
-    <h1 class="text-3xl font-bold underline bg-green-500">
-      Hello ToDo!
-    </h1>
-    <button class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700">TailwindTest</button>
-    <div>
-      <HelloWorld msg="You did it!" />
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+  <header class="w-full font-custom h-72 bg-bgImage bg-auto ">
+    <h1 class="text-center font-bold tracking-widest text-2xl text-white pt-32 pb-4 pe-72" >To Do</h1>
+    <ReadTodoList/>
   </header>
-  <RouterView />
 </template>
-<style scoped>
-</style>
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default{
     content:["./src/**/*.html", "./src/**/*.vue",],
-     darkMode: false, // or 'media' or 'class'
      theme: {
-       extend: {},
+      extend: {
+        backgroundImage: {
+          'bgImage': "url('./assets/image/bg-desktop-light.jpg')",
+        },
+        fontFamily:{
+          'custom':"url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&display=swap')",
+        },
+      },
      },
      plugins: [],
    }
