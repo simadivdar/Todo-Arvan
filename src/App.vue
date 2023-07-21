074e8cb6e87317cca918ffec5681f05b61e39700
simadivdar <simadivdar@gmail.com>
2023-07-21 11:08:54 +0330

    createAddTodo

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <script>
 import ReadTodoList from "./components/ReadTitle.vue";
+import AddTodoList from "./components/AddTitle.vue";
 export default {
   name: "App",
   components: {
-    ReadTodoList
+    ReadTodoList,
+    AddTodoList
   }
 };
 </script>
 <template>
-  <header class="w-full font-custom h-72 bg-bgImage bg-auto ">
-    <h1 class="text-center font-bold tracking-widest text-2xl text-white pt-32 pb-4 pe-72" >To Do</h1>
-    <ReadTodoList/>
+  <header class="w-full font-custom h-72 bg-bgImage bg-auto">
+    <h1 class="font-bold tracking-widest w-3/5 text-center text-2xl text-white pt-24 pb-8 ps-24 ">To Do</h1>
+    <AddTodoList />
+    <ReadTodoList />
   </header>
 </template>
 
--- src/components/AddTitle.vue
+++ src/components/AddTitle.vue
@@ -1,6 +1,30 @@
 <script>
+import axios from "axios";
+export default {
+    name: "AddTodoList",
+    data() {
+        return {
+            todoTitle: "",
+            placeholder:"Create a new Todo...",
+        };
+    },
+    methods: {
+        addTodo() {
+            axios.post(`http://localhost:3001/todos`, { "title": this.todoTitle, "done": "false" })
+                .then(response => {
+                    this.todoTitle = "";
+                })
+                .catch(e => {
+                    console.error(e);
+                })
+        }
+    }
+}
 </script>
 
 <template>
- 
+    <section class="flex justify-center">
+        <input class="p-4 rounded-md w-2/5 mb-6 " type="text" v-model="todoTitle" @keyup.enter="addTodo" @focusin="this.placeholder='Currently typing'" @focusout="this.placeholder='Create a new Todo...'"
+            aria-label="Create a new Todo" :placeholder="placeholder" />
+    </section>
 </template>
